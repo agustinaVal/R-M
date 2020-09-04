b7b8edc7649cb281b3a11b74fc3956fedccf8d88
Agustina <maria.agustina.valenti89@gmail.com>
2020-09-04 16:18:35 -0400

    primer compromiso

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+	<div v-for="(personaje,i) in personajes" :key="i" >
+		<Personaje :src="personaje.imagen" :msg="personaje.nombre" />
+	</div>
+</div>	
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import Personaje from './components/Personaje';
+export default { 
+	components: {
+		Personaje,
+	},
+	data() {
+		return {
+			personajes: [],
+		};
+	},
+	mounted() {
+		fetch('https://rickandmortyapi.com/api/character')
+			.then((resp) => resp.json())
+			.then((json) => {
+				let data = json.results;
+				data.slice(0,10).forEach((element) => {
+					let nombre = element.name;
+					let imagen = element.image;
+					this.personajes.push({ nombre, imagen });
+				});
+			})
+			.catch((error) => console.log(`Estas en un error Rick ${error}`));
+	},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+body{
+	background-image: url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png");
+	background-repeat: no-repeat;
+	background-size: cover;
 }
+div{
+	display: flex;
+	flex-flow: column;
+	color: green;
+	font-family: fantasy;
+}
+
 </style>
+
+
+
---
+++ src/components/Personaje.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img :src="src" alt="">
+    <h3> {{msg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+   name: "Personaje",
+   props: {
+       src : String,
+       msg : String,
+   }
+}
+</script>
+
+<style>
+img{
+	border-radius: 50%;
+  width: 200px;
+	height: 200px;
+	background-position: center;
+	background-size: cover;
+	background-repeat: no-repeat;
+}
+img:hover {
+	-webkit-transform: rotateY(180deg);
+	-webkit-transform-style: preserve-3d;
+	transform: rotateY(180deg);
+	transform-style: preserve-3d;
+}
+h3{
+  padding-left: 50px;
+}
+
+</style>
